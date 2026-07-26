b922c1740cb226f229a27373efaa3024e57f3d71
OrestisTheGreat <tr_voter1@outlook.com>
2026-07-26 03:15:41 +0300

    Create countries.js

---
+++ countries.js
@@ -0,0 +1,33 @@
+const countries = {
+
+    Belgium: {
+        frog: "Bart the Belgian Frog",
+        birthdayWish: "Gelukkige verjaardag! 🐸",
+        trivia: "Belgium has three official languages.",
+        neighbours: [
+            "France"
+        ]
+    },
+
+
+    France: {
+        frog: "François the Frog",
+        birthdayWish: "Joyeux anniversaire! 🐸",
+        trivia: "France is the largest country in the EU by area.",
+        neighbours: [
+            "Belgium",
+            "Germany"
+        ]
+    },
+
+
+    Germany: {
+        frog: "Friedrich the Frog",
+        birthdayWish: "Alles Gute zum Geburtstag! 🐸",
+        trivia: "Germany has over 20,000 castles.",
+        neighbours: [
+            "France"
+        ]
+    }
+
+};z
