a7a486a3bf2166cf099cb7a38c3943dbb9d0a9f5
OrestisTheGreat <tr_voter1@outlook.com>
2026-07-26 03:48:23 +0300

    Update map.js

--- map.js
+++ map.js
@@ -1,42 +1,42 @@
 const mapPositions = {
 
     Ireland: {
-        x: 300,
-        y: 950
+        x: 350,
+        y: 850
     },
 
     Portugal: {
-        x: 450,
-        y: 1800
+        x: 500,
+        y: 1900
     },
 
     Spain: {
-        x: 700,
+        x: 750,
         y: 1700
     },
 
     France: {
-        x: 1000,
-        y: 1300
+        x: 1050,
+        y: 1350
     },
 
     Belgium: {
-        x: 1150,
+        x: 1200,
         y: 1050
     },
 
     Netherlands: {
-        x: 1200,
+        x: 1250,
         y: 850
     },
 
     Luxembourg: {
-        x: 1150,
+        x: 1200,
         y: 1200
     },
 
     Germany: {
-        x: 1450,
+        x: 1500,
         y: 1050
     },
 
@@ -45,13 +45,38 @@ const mapPositions = {
         y: 600
     },
 
+    Sweden: {
+        x: 1750,
+        y: 300
+    },
+
+    Finland: {
+        x: 2250,
+        y: 250
+    },
+
+    Estonia: {
+        x: 2200,
+        y: 500
+    },
+
+    Latvia: {
+        x: 2150,
+        y: 650
+    },
+
+    Lithuania: {
+        x: 2100,
+        y: 800
+    },
+
     Poland: {
         x: 1800,
-        y: 900
+        y: 950
     },
 
     Czechia: {
-        x: 1600,
+        x: 1650,
         y: 1250
     },
 
@@ -66,7 +91,7 @@ const mapPositions = {
     },
 
     Hungary: {
-        x: 1850,
+        x: 1900,
         y: 1550
     },
 
@@ -76,63 +101,38 @@ const mapPositions = {
     },
 
     Croatia: {
-        x: 1750,
-        y: 1700
+        x: 1800,
+        y: 1750
     },
 
     Italy: {
         x: 1500,
-        y: 2100
+        y: 2200
     },
 
     Greece: {
-        x: 2150,
-        y: 2400
+        x: 2200,
+        y: 2450
     },
 
     Bulgaria: {
-        x: 2150,
-        y: 1900
+        x: 2200,
+        y: 2000
     },
 
     Romania: {
         x: 2300,
-        y: 1700
-    },
-
-    Lithuania: {
-        x: 2100,
-        y: 700
-    },
-
-    Latvia: {
-        x: 2150,
-        y: 520
-    },
-
-    Estonia: {
-        x: 2200,
-        y: 330
-    },
-
-    Sweden: {
-        x: 1750,
-        y: 350
-    },
-
-    Finland: {
-        x: 2250,
-        y: 200
+        y: 1750
     },
 
     Cyprus: {
-        x: 2650,
-        y: 2800
+        x: 2700,
+        y: 2850
     },
 
     Malta: {
         x: 1450,
-        y: 2750
+        y: 2800
     }
 
 };
