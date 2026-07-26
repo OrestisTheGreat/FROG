479f6665ef8115e2602801a70d779b61f125d922
OrestisTheGreat <tr_voter1@outlook.com>
2026-07-26 03:43:39 +0300

    Create map.js

---
+++ map.js
@@ -0,0 +1,138 @@
+const mapPositions = {
+
+    Ireland: {
+        x: 300,
+        y: 950
+    },
+
+    Portugal: {
+        x: 450,
+        y: 1800
+    },
+
+    Spain: {
+        x: 700,
+        y: 1700
+    },
+
+    France: {
+        x: 1000,
+        y: 1300
+    },
+
+    Belgium: {
+        x: 1150,
+        y: 1050
+    },
+
+    Netherlands: {
+        x: 1200,
+        y: 850
+    },
+
+    Luxembourg: {
+        x: 1150,
+        y: 1200
+    },
+
+    Germany: {
+        x: 1450,
+        y: 1050
+    },
+
+    Denmark: {
+        x: 1500,
+        y: 600
+    },
+
+    Poland: {
+        x: 1800,
+        y: 900
+    },
+
+    Czechia: {
+        x: 1600,
+        y: 1250
+    },
+
+    Austria: {
+        x: 1650,
+        y: 1450
+    },
+
+    Slovakia: {
+        x: 1800,
+        y: 1400
+    },
+
+    Hungary: {
+        x: 1850,
+        y: 1550
+    },
+
+    Slovenia: {
+        x: 1650,
+        y: 1650
+    },
+
+    Croatia: {
+        x: 1750,
+        y: 1700
+    },
+
+    Italy: {
+        x: 1500,
+        y: 2100
+    },
+
+    Greece: {
+        x: 2150,
+        y: 2400
+    },
+
+    Bulgaria: {
+        x: 2150,
+        y: 1900
+    },
+
+    Romania: {
+        x: 2300,
+        y: 1700
+    },
+
+    Lithuania: {
+        x: 2100,
+        y: 700
+    },
+
+    Latvia: {
+        x: 2150,
+        y: 520
+    },
+
+    Estonia: {
+        x: 2200,
+        y: 330
+    },
+
+    Sweden: {
+        x: 1750,
+        y: 350
+    },
+
+    Finland: {
+        x: 2250,
+        y: 200
+    },
+
+    Cyprus: {
+        x: 2650,
+        y: 2800
+    },
+
+    Malta: {
+        x: 1450,
+        y: 2750
+    }
+
+};
