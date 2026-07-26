37af81f85c6a96043cf6fdc38031b46bf27a26cb
OrestisTheGreat <tr_voter1@outlook.com>
2026-07-26 03:10:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const startButton = document.getElementById("startButton");
 
 startButton.addEventListener("click", function () {
-    alert("Welcome to Frog Leaders of Europe!");
+    alert("🐸 Welcome to Frog Leaders of Europe!");
 });
