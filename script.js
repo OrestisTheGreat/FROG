310047f1daf9a85db8b01bf6e26cfd66549c4bae
OrestisTheGreat <tr_voter1@outlook.com>
2026-07-26 03:20:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,56 @@
 const startButton = document.getElementById("startButton");
 
+const gameArea = document.getElementById("gameArea");
+const countryName = document.getElementById("countryName");
+const message = document.getElementById("message");
+const moveButtons = document.getElementById("moveButtons");
+
+
+let currentCountry = "Belgium";
+
+
 startButton.addEventListener("click", function () {
-    alert("🐸 Welcome to Frog Leaders of Europe!");
+
+    startButton.style.display = "none";
+    gameArea.style.display = "block";
+
+    showCountry();
+
 });
+
+
+function showCountry() {
+
+    countryName.textContent = "🇪🇺 " + currentCountry;
+
+
+    message.textContent =
+        countries[currentCountry].frog +
+        " says: " +
+        countries[currentCountry].birthdayWish;
+
+
+    moveButtons.innerHTML = "";
+
+
+    countries[currentCountry].neighbours.forEach(function(country) {
+
+        const button = document.createElement("button");
+
+        button.textContent = "Travel to " + country;
+
+
+        button.addEventListener("click", function() {
+
+            currentCountry = country;
+
+            showCountry();
+
+        });
+
+
+        moveButtons.appendChild(button);
+
+    });
+
+}
